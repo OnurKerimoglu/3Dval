e1eaedac11d38492191651a21068aa69c2ff547b
Onur.Kerimoglu <onur.kerimoglu@hzg.de>
2018-03-24 11:13:43 +0100

    stations_main: define vars to extract, other minor adjustments

--- stations_main.py
+++ stations_main.py
@@ -5,10 +5,11 @@ Created on Thu Jun  8 15:02:46 2017
 Main file for stations validation.
 Control of structre, call reading obs. model and plot.
 
-@author: kerimoglu.o@gmail.com, ivan.kuznetsov@gmail.com
+@author: kerimoglu.o@gmail.com
 
 """
 import os,sys
+import warnings
 import datetime
 import stations_readobs as SRO
 import stations_readsim as SRS
@@ -20,51 +21,52 @@ importlib.reload(SRO)
 importlib.reload(SRS)
 importlib.reload(SP)
 
-pathreg = {'onur': {'GF': '/home/onur/WORK/projects/GB/maecs/3d/sns144-M161117n-P161118-bdyi3-z01mm-wAtmN/sns144-M161117n-P161118-bdyi3-z01mm-wAtmN-mergedextract_phys_2010-2014_zSB.nc',
-                    #'GF-old': '/home/onur/WORK/projects/GB/maecs/3d/sns144-M161117-P161118/sns144-M161117-P161118-mergedextract_phys_zSB_2006-2010.nc',
-                    'plotrootpath':'/home/onur/WORK/projects/GB/maecs/3d/sns144-M161117n-P161118-bdyi3-z01mm-wAtmN',
-                    'rootpath': './',
-                    'emodnet': '?',
-                    'cosyna': '/home/onur/WORK/projects/GB/data/stations/stations_COSYNA',
-                    'marnet': '/home/onur/WORK/projects/GB/data/stations/stations_MARNET'},
-           'ivan': {'rootpath': './',
-                    'emodnet': '/workm/data/INSITU_NWS_NRT_OBSERVATIONS_013_036/history/mooring/',
-                    'cosyna': '?',
-                    'marnet': '?',}
+pathreg = {'onur': {#'GF': '/home/onur/WORK/projects/2013/maecs/sns144-M180109-nBF-Pbg2017-B180106-vsdetp4b1/sns144-M180109-nBF-Pbg2017-B180106-vsdetp4b1-1113_phys_zSB.nc',
+                    #'plotrootpath':'/home/onur/WORK/projects/2013/maecs/sns144-M180106-nBF-Pbg2017-B180106-vsdetp4b1/','
+                    #'GF': '/home/onur/WORK/projects/2013/maecs/sns144-M180109-nFpB-Pbg2017-B180106-vsdetp4b1/sns144-M180109-nFpB-Pbg2017-B180106-vsdetp4b1-1114_phys_zSB.nc',
+                    'GF': '/home/onur/WORK/projects/2013/maecs/sns144-M180109-nFpB-Pbg2017-B180106-vsdetp4b1/sns144-M180109-nFpB-Pbg2017-B180106-vsdetp4b1-1114_chl_zSB.nc',
+                    'plotrootpath':'/home/onur/WORK/projects/2013/maecs/sns144-M180109-nFpB-Pbg2017-B180106-vsdetp4b1/',
+                    'pickledobspath': './',
+                    'BGC':    '/home/onur/WORK/projects/GB/data/stations/individual/BGC/',
+                    'BSH':    '/home/onur/WORK/projects/GB/data/stations/individual/BSH/',
+                    'cosyna': '/home/onur/WORK/projects/GB/data/stations/COSYNA/proc/nc'},
+           'newuser': {}
            }
 
 def main():
     #PARAMETERS:
     # general
-    user='onur' #ivan,onur
-    depthints={'surface':[0,5]} #,'bottom':[5,0]} #,'bottom':[5,0]} #for bottom, depthint is relative to bottom depth
-    timeint = [datetime.datetime(2010, 1, 1,0,0,0), datetime.datetime(2014, 12, 31,23,59,59)]
+    user='onur'
+    #vars=['temp','salt','DOs']
+    vars = ['DIN','DIP','Chl']
+    depthints={'surface':[0,10]} #,'bottom':[10,0]} #for bottom, depthint is relative to bottom depth
+    timeint = [datetime.datetime(2012, 1, 1,0,0,0), datetime.datetime(2014, 12, 31,23,59,59)]
     ##timeint = [datetime.datetime(2000, 1, 1, 0, 0, 0), datetime.datetime(2010, 12, 31, 23, 59, 59)]
-    # regarding observations. todo: place in a dictionary, specify variables to extract
-    statsets = ['cosyna'] #,'marnet','cosyna'
+    # regarding observations.
+    statsets = ['BGC'] #'cosyna', 'BSH', 'BGC'
     stations = []
     readobsraw=False #i.e., if the pickle file should be ignored
-    # regarding simulations. todo: place in a dictionary, specify variables to extract
+    # regarding simulations.
     sims2plot = ['GF']
-    readsimraw=False #i.e., if the pickle file should be ignored
+    readsimraw=True #i.e., if the pickle file should be ignored
     simdomain=''
-    meth2D='int_tree'
+    meth2D='pretree'
     #regarding plots:
-    plotopts={'TS':True,'TSstyle':'TSdefault','varns':['temp','salt'],'sims2plot':sims2plot}
+    plotopts={'TS':True,'TSstyle':'TSdefault','varns':vars,'sims2plot':sims2plot}
 
     #READ OBSERVATIONS
-    obs=SRO.readobs(pathreg[user],readobsraw,statsets,stations,timeint,depthints)
+    obs=SRO.readobs(pathreg[user],readobsraw,statsets,stations,timeint,depthints,vars)
 
     #READ SIMULATIONS
     simset={}    
     for simno, simname in enumerate(sims2plot):
-        sim=SRS.readsim(pathreg[user],simname,readsimraw,simdomain,meth2D,statsets,timeint,depthints,obs)
+        sim=SRS.readsim(pathreg[user],simname,readsimraw,simdomain,meth2D,statsets,timeint,depthints,obs,vars)
         simset[simname]=sim
 
     #print(simset)
 
     #PLOTS
-    SP.stations_plots(plotopts, obs, simset, pathreg[user]['plotrootpath'], stations, timeint, depthints)
+    SP.stations_plots(plotopts, obs, simset, pathreg[user]['plotrootpath'], statsets, stations, timeint, depthints)
 
 if __name__=='__main__':
     main()
